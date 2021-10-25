3cbe24e50836bde90036d654d6087256665554a4
Rupali Margale <69862836+rupalimargale99@users.noreply.github.com>
2021-10-26 01:01:46 +0530

    Add files via upload

---
+++ EFCFMastek/EFCFMastek/EFCFContext.cs
@@ -0,0 +1,19 @@
+﻿using System; 
+using System.Collections.Generic; 
+using System.Linq; using System.Text; 
+using System.Threading.Tasks; 
+using Microsoft.EntityFrameworkCore; 
+using Microsoft.EntityFrameworkCore.ChangeTracking; 
+
+namespace EFCFMastek 
+{ 
+    public class EFCFContext:DbContext
+    { 
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; } 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+            optionsBuilder.UseSqlServer("SerVer = (localdb)\\MSSQLLocalDB;Database = EFCFDB;Integrated Security = True;");
+        } 
+    } 
+}
