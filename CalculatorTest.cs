e6a4e10d1d529a3a52e665848b867747b9e8a710
Rupali Margale <69862836+rupalimargale99@users.noreply.github.com>
2021-10-26 01:23:35 +0530

    NUnitDemo

---
+++ CalculatorTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using NUnit.Framework;
+using NUnitDemoLib;
+
+
+namespace NUnitTestLib
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        //setup-Run before every test method
+        //Teardown-Run after every test method
+
+        Calculator calculator;
+        [SetUp]
+        public void Setup()
+        {
+            calculator = new Calculator();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            //write code which runs after every test method
+        }
+
+        [Test]
+        public void Test_Addition_with_Valid_integers()
+        {
+           // Calculator calculator = new Calculator();
+            int result = calculator.Addition(5, 8);
+            //Assert.AreEqual(expected,actual);
+            Assert.AreEqual(13, result);
+        }
+
+        //batch testing
+        [TestCase(1,2,3)]
+        [TestCase(0,0,0)]
+        [TestCase(-1,-1,-2)]
+
+        public void Test_Addition_multiple(int first,int second,int expected)
+        {
+            //Calculator calculator = new Calculator();
+            int result = calculator.Addition(first, second);
+            Assert.AreEqual(expected, result);
+        }
+
+        //Exception Test
+        [Test]
+        public void Test_Subtraction_ArgumentException()
+        {
+            //Calculator calculator = new Calculator();
+            //sub class instance is allowed
+            //Assert.Catch<SystemException>(() => calculator.Subtraction(3, 5));
+
+            //exact exception match
+            //Assert.Throws<SystemException>(() => calculator.Subtraction(3, 5));
+           Assert.Throws<ArgumentException>(() => calculator.Subtraction(3, 5));
+        }
+    }
+}
