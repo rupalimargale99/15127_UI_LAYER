a7d41267578853f55562abc9d0f05d4ced0121fc
Rupali Margale <69862836+rupalimargale99@users.noreply.github.com>
2021-10-26 00:47:11 +0530

    Add files via upload

---
+++ Database/DBDemo/DataBaseApp/DataCRUDForm.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataLibrary;
+
+namespace DataBaseApp
+{
+    public partial class DataCRUDForm : Form
+    {
+        EmpDataStore dataStore = new EmpDataStore(@"server=(localdb)\MSSQLLocalDB;database=Training;Integrated Security=True");
+        public DataCRUDForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DataCRUDForm_Load(object sender, EventArgs e)
+        {
+            dgData.DataSource = dataStore.GetEmps();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+
+            txtEmpNo.Clear();
+            txtEmpName.Clear();
+            txtHireDate.Clear();
+            txtSalary.Clear();
+        }
+
+        private void btnAddEmp_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Emp newEmp = new Emp();
+                newEmp.EmpNo = int.Parse(txtEmpNo.Text);
+                newEmp.EmpName = txtEmpName.Text;
+                newEmp.HireDate = Convert.ToDateTime(txtHireDate.Text);
+                newEmp.Salary = decimal.Parse(txtSalary.Text);
+
+                int count = dataStore.AddEmp(newEmp);
+
+                if(count==1)
+                {
+                    MessageBox.Show("Record inserted");
+                    dgData.DataSource=dataStore.GetEmps();
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                int count = dataStore.RemoveEmp(int.Parse(txtEmpNo.Text));
+
+                if (count == 1)
+                {
+                    MessageBox.Show("Record removed");
+                    dgData.DataSource = dataStore.GetEmps();
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Emp newEmp = new Emp();
+                newEmp.EmpNo = int.Parse(txtEmpNo.Text);
+                newEmp.EmpName = txtEmpName.Text;
+                newEmp.HireDate = Convert.ToDateTime(txtHireDate.Text);
+                newEmp.Salary = decimal.Parse(txtSalary.Text);
+
+                int count = dataStore.EditEmp(newEmp);
+
+                if (count == 1)
+                {
+                    MessageBox.Show("Record updated");
+                    dgData.DataSource = dataStore.GetEmps();
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (txtEmpNo.Text == string.Empty)
+            {
+                return;
+            }
+
+            try
+            {
+                int empno = int.Parse(txtEmpNo.Text);
+                Emp emp = dataStore.GetEmpByNo(empno);
+               
+
+                if (emp!=null)
+                {
+                    txtEmpName.Text = emp.EmpName;
+                    txtHireDate.Text = emp.HireDate.ToString();
+                    txtSalary.Text = emp.Salary.ToString();
+                    
+                }
+                else
+                {
+                    MessageBox.Show($"Employee by no{empno} not found");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ Database/DBDemo/DataBaseApp/FrmLogin.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace DataBaseApp
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSignin = new System.Windows.Forms.Button();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSignin
+            // 
+            this.btnSignin.Location = new System.Drawing.Point(287, 272);
+            this.btnSignin.Name = "btnSignin";
+            this.btnSignin.Size = new System.Drawing.Size(112, 34);
+            this.btnSignin.TabIndex = 0;
+            this.btnSignin.Text = "Signin";
+            this.btnSignin.UseVisualStyleBackColor = true;
+            this.btnSignin.Click += new System.EventHandler(this.btnSignin_Click_1);
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(159, 83);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(94, 25);
+            this.lblUserName.TabIndex = 1;
+            this.lblUserName.Text = "UserName";
+            this.lblUserName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(169, 140);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(87, 25);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Password";
+            this.lblPassword.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Font = new System.Drawing.Font("Segoe UI Black", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblMsg.Location = new System.Drawing.Point(271, 206);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(77, 38);
+            this.lblMsg.TabIndex = 3;
+            this.lblMsg.Text = "Msg";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(346, 83);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(150, 31);
+            this.txtUserName.TabIndex = 4;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(346, 140);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(150, 31);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 450);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.btnSignin);
+            this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmLogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSignin;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+    }
+}
