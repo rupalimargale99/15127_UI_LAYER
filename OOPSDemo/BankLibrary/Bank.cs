73dcedc7d08f0c5ec96ba772c6f58c90d7021c00
Rupali Margale <69862836+rupalimargale99@users.noreply.github.com>
2021-10-26 01:18:23 +0530

    Add files via upload

---
+++ OOPSDemo/BankLibrary/Bank.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace BankLibrary
+{
+    public abstract class Bank
+    {
+        #region Fiels & Properties
+
+        private string accHolderName;
+
+
+        public string AccHolderName
+        {
+            get { return accHolderName; }
+            set { accHolderName = value; }
+        }
+        private double balance;
+        public double Balance
+        {
+            get { return balance; }
+            protected set { balance = value; }
+        }
+        #endregion
+
+        #region Methods
+
+        public void Deposit (double amount)
+        {
+            Balance += amount;
+        }
+        public virtual void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+
+        public override string ToString()
+        {
+            //traditional
+            //return string.Format ("AccountHolderName:" + AccHolderName + "Balance:" + Balance);
+
+            //formated string
+            //return string.Format("AccountHolderName: {0} Balance: {1}", AccHolderName, Balance);
+
+            //string interpolation
+            return string.Format($"Account Holder Name: {AccHolderName} Balance: {Balance}");
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public Bank()
+        {
+            Balance = 1000;
+        }
+
+        public Bank(String name,double amount)
+        {
+            this.AccHolderName = name;
+            this.Balance = amount;
+        }
+        #endregion
+    }
+}
---
+++ OOPSDemo/BankLibrary/Savings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public class Savings : Bank, IInterest
+    {
+        public Savings()
+        {
+
+        }
+        public Savings(string name, double amt) : base(name, amt)
+        {
+
+        }
+
+        public double CalculateInterest()
+        {
+            return Balance * .1;
+        }
+
+        public sealed override void Withdraw(double amount)
+        {
+            if ((Balance - amount) > 0)
+            {
+                //allow to withdraw
+                Balance -= amount;
+            }
+            else
+            {
+                throw new BalanceException("Balance not enough. Cannot withdraw");
+            }
+        }
+
+   
+    }
+    public class Current:Bank
+    {
+        public override void Withdraw(double amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
